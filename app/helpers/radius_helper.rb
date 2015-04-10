f03c466f05d75198344b7db6d10d508e87012ca0
Christopher Sexton <github@codeography.com>
2015-04-10 14:29:54 -0400

    Allow kracken server to include the masquerade bar
    Fixes a few bugs when PROVIDER_URL is not set.

--- app/helpers/radius_helper.rb
+++ app/helpers/radius_helper.rb
@@ -1,9 +1,9 @@
 module RadiusHelper
   def kracken_url
-    if defined? Kracken
+    if defined? Kracken::PROVIDER_URL
       Kracken::PROVIDER_URL
     else
-      ENV['RADIUS_OAUTH_PROVIDER_URL'].fetch { "https://account.radiusnetworks.com" }
+      ENV.fetch('RADIUS_OAUTH_PROVIDER_URL') { "https://account.radiusnetworks.com" }
     end
   end
 
@@ -12,4 +12,19 @@ module RadiusHelper
       content_tag(:div, ENV['ENVIRONMENT_NAME'], :id => 'environment_ribbon')
     end
   end
+
+  def masquerading?
+    #session[:admin_user_id]
+    cookies[:_radius_user_masquerade] && current_user
+  end
+
+  def end_masquerade_path
+    if respond_to?(:masquerade_destroy_path)
+      masquerade_destroy_path
+    else
+      "#{kracken_url}/admin/masquerade/destroy"
+    end
+  end
+
+
 end
