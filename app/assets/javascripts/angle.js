81c356c8c5c771e467fb1b4eda51accb3cc16fb7
Christopher Sexton <github@codeography.com>
2016-02-23 21:16:33 -0500

    Initial Import of new Bootstrap Theme
    There are quite a large number of css and js plugins that the theme
    included that seem unnecessary, but might provide good reference in the
    future. So I am checking in the base theme with minimal changes. The
    idea is that we can have a suite of components and plugins that we might
    use for building out the different Radius apps.
    
    Main changes include:
    
    - renaming bower_components to components
    - create base angle.js and angle.css that can be included in downstream
      apps
    - bump the version number
    - manually moving the fonts to the fonts and stylesheets folders (used
      erb to re-write the .css files for the proper asset paths
    - Stole the `fa_icon` helper from https://github.com/bokmann/font-awesome-rails

---
+++ app/assets/javascripts/angle.js
@@ -0,0 +1,11 @@
+//= require_tree ./angle/
+//= require modernizr/modernizr.custom
+//= require jquery/dist/jquery
+//= require bootstrap/dist/js/bootstrap
+//= require jQuery-Storage-API/jquery.storageapi
+//= require jquery.easing/js/jquery.easing
+//= require animo.js/animo
+//= require slimScroll/jquery.slimscroll.min
+//= require screenfull/dist/screenfull
+//= require jquery-localize-i18n/dist/jquery.localize
+
--- lib/radius/rails/version.rb
+++ lib/radius/rails/version.rb
@@ -1,5 +1,5 @@
 module Radius
   module Rails
-    VERSION = "0.0.6"
+    VERSION = "1.0.0"
   end
 end
