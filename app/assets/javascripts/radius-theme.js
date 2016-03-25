c554f8e64d1916a877ea3e83532a21935166b18b
Alastair Hewitt <al@radiusnetworks.com>
2016-03-25 11:53:07 -0400

    add chosen and moment to JS imports

--- app/assets/javascripts/radius-theme.js
+++ app/assets/javascripts/radius-theme.js
@@ -4,6 +4,10 @@
 //= require jQuery-Storage-API/jquery.storageapi
 //= require jquery.easing/js/jquery.easing
 //= require animo.js/animo
+//= require chosen_v1.2.0/chosen.jquery
+//= require moment/moment
+//= require moment-timezone/moment-timezone
+//= require eonasdan-bootstrap-datetimepicker
 //= require slimScroll/jquery.slimscroll.min
 //= require screenfull/dist/screenfull
 //= require_tree ./radius-theme/
