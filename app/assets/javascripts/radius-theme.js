9ebb1c94053c7174606abdd888a68d9f635668fc
Alastair Hewitt <al@radiusnetworks.com>
2016-04-05 13:43:23 -0400

    inject timezone data into moment-timezone.js

--- app/assets/javascripts/radius-theme.js
+++ app/assets/javascripts/radius-theme.js
@@ -6,8 +6,9 @@
 //= require animo.js/animo
 //= require chosen_v1.2.0/chosen.jquery
 //= require moment/moment
-//= require moment-timezone/moment-timezone
+//= require moment-timezone/moment-timezone-with-data
 //= require eonasdan-bootstrap-datetimepicker
 //= require slimScroll/jquery.slimscroll.min
 //= require screenfull/dist/screenfull
 //= require_tree ./radius-theme/
+
